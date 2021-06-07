0b62b687b483863882461c76da9f9eec4db6a247
playstation7 <neket.1992@mail.ru>
2021-06-07 23:56:43 +0300

    First

--- diplom/settings.py
+++ diplom/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-eapmry^qk%(ny8lx)iu!21sf(^ocb1l+4_8brko@^jgyn-lin8
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
